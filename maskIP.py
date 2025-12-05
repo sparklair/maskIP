ca94df66f76c6f10a03f8a4214380b7285d83137
Vladimir <anonym@anonym>
2025-12-05 04:30:47 +0300

    Added oct ip

--- maskIP.py
+++ maskIP.py
@@ -2,13 +2,14 @@ import sys
 
 ip_address = sys.argv[1].split('.')
 
-changed_ip = 0
+changed_ip_1 = 0
+changed_ip_2 = ''
 power = 3
 
 for octet in ip_address:
-    changed_ip += int(octet) * 256**power
+    changed_ip_1 += int(octet) * 256**power
     power -= 1
+changed_ip_2 = f'0{changed_ip_1:o}'
 
-sys.stdout.write(str(changed_ip))
-
-
+sys.stdout.write(f'{changed_ip_1}\n')
+sys.stdout.write(changed_ip_2)
